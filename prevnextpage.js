bdf421edbc1cdd86f7f7e49fb0288fcebd9b43a3
tahtektux <tahtektux@gmail.com>
2019-04-02 18:55:46 +0700

    Add files via upload

---
+++ prevnextpage.js
@@ -0,0 +1,19 @@
+<script type='text/javascript'>
+
+if (typeof(jQuery) == 'undefined') {document.write("<scr" + "ipt type=\"text/javascript\" src=\"//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js\"></scr" + "ipt>");}
+
+</script>
+<script type='text/javascript'>
+
+(function($){  
+    var newerLink = $('a.blog-pager-newer-link');
+    var olderLink = $('a.blog-pager-older-link');
+    $.get(newerLink.attr('href'), function (data) {
+     newerLink.html($(data).find('.post h1.post-title').text());  
+    },"html");
+    $.get(olderLink.attr('href'), function (data2) {
+     olderLink.html($(data2).find('.post h1.post-title').text());  
+    },"html");
+})(jQuery);
+
+</script>
